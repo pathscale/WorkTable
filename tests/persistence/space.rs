1d592a25881c504fdfa2ca5372cd45dd25749720
Handy-caT <37216852+Handy-caT@users.noreply.github.com>
2024-12-27 00:13:03 +0300

    add test for data sync

--- tests/persistence/space.rs
+++ tests/persistence/space.rs
@@ -1,13 +1,30 @@
-use crate::persistence::{SizeTestRow, SizeTestWrapper, TestPersistRow, TestPersistSpace};
+use crate::persistence::{TestPersistRow, TestPersistSpace, TEST_PERSIST_PAGE_SIZE};
+use data_bucket::Link;
+use std::fs::OpenOptions;
+use worktable::prelude::SpaceData;
 
 #[test]
 fn test_file_write() {
     let row = TestPersistRow {
-        another: 100,
-        id: 1000,
+        another: u64::MAX - 1,
+        id: u128::MAX - 1,
     };
     let bytes = rkyv::to_bytes::<rkyv::rancor::Error>(&row).unwrap();
-    let space = TestPersistSpace {
-        file: 
-    }
+    let mut space = TestPersistSpace::<{ TEST_PERSIST_PAGE_SIZE }> {
+        file: OpenOptions::new()
+            .write(true)
+            .open("tests/data/test_persist.wt")
+            .unwrap(),
+    };
+
+    space
+        .save_data(
+            Link {
+                page_id: 3.into(),
+                length: 32,
+                offset: 0,
+            },
+            &*bytes,
+        )
+        .unwrap();
 }
