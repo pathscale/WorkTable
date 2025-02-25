807db51a9fe8082faad46510f1947451c44732ed
Kira Sotnikov <s8tn1kv@gmail.com>
2025-02-25 15:13:59 +0300

    Small fix for locks impl gen

--- codegen/src/worktable/generator/locks.rs
+++ codegen/src/worktable/generator/locks.rs
@@ -5,26 +5,36 @@ use quote::quote;
 
 impl Generator {
     pub fn gen_locks_def(&self) -> TokenStream {
-        let type_def = self.gen_locks_type();
-        let impl_def = self.gen_locks_impl();
+        let type_ = self.gen_locks_type();
+        let impl_ = self.gen_locks_impl();
 
         quote! {
-            #type_def
-            #impl_def
+            #type_
+            #impl_
+
         }
     }
 
     fn gen_locks_type(&self) -> TokenStream {
         let name_generator = WorktableNameGenerator::from_table_name(self.name.to_string());
         let lock_ident = name_generator.get_lock_type_ident();
-        let row_locks = self.gen_row_locks();
+
+        let rows: Vec<_> = self
+            .columns
+            .columns_map
+            .keys()
+            .map(|i| {
+                let name = Ident::new(format!("{i}_lock").as_str(), Span::mixed_site());
+                quote! { #name: Option<std::sync::Arc<Lock>>, }
+            })
+            .collect();
 
         quote! {
              #[derive(Debug, Clone)]
              pub struct #lock_ident {
                 id: u16,
                 lock: Option<std::sync::Arc<Lock>>,
-                #(#row_locks)*
+                #(#rows)*
             }
         }
     }
@@ -33,45 +43,23 @@ impl Generator {
         let name_generator = WorktableNameGenerator::from_table_name(self.name.to_string());
         let lock_ident = name_generator.get_lock_type_ident();
 
-        let new_fn = self.gen_lock_new_fn();
-        let row_with_lock = self.gen_row_with_lock();
-        let row_lock_await = self.gen_row_lock_await();
-        let row_unlock = self.gen_row_unlock();
+        let new_fn = self.gen_new_fn();
+        let with_lock_fn = self.gen_with_lock_fn();
+        let lock_await_fn = self.gen_lock_await_fn();
+        let unlock_fn = self.gen_unlock_fn();
 
         quote! {
             impl #lock_ident {
                 #new_fn
-
-                pub fn with_lock(lock_id: u16) -> Self {
-                    Self {
-                        id: lock_id,
-                        lock: Some(std::sync::Arc::new(Lock::new())),
-                        #(#row_with_lock),*
-                    }
-                }
-
-                pub fn unlock(&self) {
-                    if let Some(lock) = &self.lock {
-                        lock.unlock();
-                    }
-                    #(#row_unlock)*
-                }
-
-                pub async fn lock_await(&self) {
-                    let mut futures = Vec::new();
-
-                    if let Some(lock) = &self.lock {
-                        futures.push(lock.as_ref());
-                    }
-                    #(#row_lock_await)*
-                    futures::future::join_all(futures).await;
-                }
+                #with_lock_fn
+                #lock_await_fn
+                #unlock_fn
             }
         }
     }
 
-    fn gen_lock_new_fn(&self) -> TokenStream {
-        let fields = self
+    fn gen_new_fn(&self) -> TokenStream {
+        let rows: Vec<_> = self
             .columns
             .columns_map
             .keys()
@@ -86,36 +74,37 @@ impl Generator {
                 Self {
                     id: lock_id,
                     lock: None,
-                    #(#fields),*
+                    #(#rows),*
                 }
             }
         }
     }
 
-    fn gen_row_locks(&self) -> Vec<TokenStream> {
-        self.columns
-            .columns_map
-            .keys()
-            .map(|i| {
-                let name = Ident::new(format!("{i}_lock").as_str(), Span::mixed_site());
-                quote! { #name: Option<std::sync::Arc<Lock>>, }
-            })
-            .collect()
-    }
-
-    fn gen_row_with_lock(&self) -> Vec<TokenStream> {
-        self.columns
+    fn gen_with_lock_fn(&self) -> TokenStream {
+        let rows: Vec<_> = self
+            .columns
             .columns_map
             .keys()
             .map(|i| {
                 let col = Ident::new(format!("{i}_lock").as_str(), Span::mixed_site());
                 quote! { #col: Some(std::sync::Arc::new(Lock::new())) }
             })
-            .collect()
+            .collect();
+
+        quote! {
+             pub fn with_lock(lock_id: u16) -> Self {
+                Self {
+                    id: lock_id,
+                    lock: Some(std::sync::Arc::new(Lock::new())),
+                    #(#rows),*
+                }
+            }
+        }
     }
 
-    fn gen_row_lock_await(&self) -> Vec<TokenStream> {
-        self.columns
+    fn gen_lock_await_fn(&self) -> TokenStream {
+        let rows: Vec<_> = self
+            .columns
             .columns_map
             .keys()
             .map(|col| {
@@ -126,11 +115,23 @@ impl Generator {
                    }
                 }
             })
-            .collect()
+            .collect();
+        quote! {
+             pub async fn lock_await(&self) {
+                let mut futures = Vec::new();
+
+                if let Some(lock) = &self.lock {
+                    futures.push(lock.as_ref());
+                }
+                #(#rows)*
+                futures::future::join_all(futures).await;
+            }
+        }
     }
 
-    fn gen_row_unlock(&self) -> Vec<TokenStream> {
-        self.columns
+    fn gen_unlock_fn(&self) -> TokenStream {
+        let rows: Vec<_> = self
+            .columns
             .columns_map
             .keys()
             .map(|col| {
@@ -141,6 +142,16 @@ impl Generator {
                      }
                 }
             })
-            .collect()
+            .collect();
+
+        quote! {
+            pub fn unlock(&self) {
+                if let Some(lock) = &self.lock {
+                    lock.unlock();
+                }
+                #(#rows)*
+            }
+
+        }
     }
 }
