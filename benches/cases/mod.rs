e4c451d505cefdd0bf0eea3054f098950153428f
meh <no-reply@pathscale.com>
2026-09-01 05:41:00 +0700

    Benchmark non-unique arctic against WTI String adjacency

--- benches/cases/mod.rs
+++ benches/cases/mod.rs
@@ -1,5 +1,6 @@
 pub mod full_featured;
 pub mod non_unique_index;
+pub mod nonunique_arctic_vs_wti;
 pub mod partition_routing;
 pub mod simple;
 pub mod unique_index;
--- benches/worktable_benchmarks.rs
+++ benches/worktable_benchmarks.rs
@@ -7,6 +7,7 @@ criterion_main!(
     cases::simple::simple_benchmarks,
     cases::unique_index::unique_index_benchmarks,
     cases::non_unique_index::non_unique_index_benchmarks,
+    cases::nonunique_arctic_vs_wti::nonunique_arctic_vs_wti_benchmarks,
     cases::full_featured::full_featured_benchmarks,
     cases::update_contention::update_contention_benchmarks,
     cases::partition_routing::partition_routing_benchmarks,
